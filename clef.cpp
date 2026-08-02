d846f23466e426ae19809574b90c6f4d223c11d3
NotAlive24 <notalive7593@duck.com>
2026-08-02 09:55:21 +0530

    File check and create

--- clef.cpp
+++ clef.cpp
@@ -1,7 +1,44 @@
+#include <cstdlib>
+#include <fstream>
 #include <iostream>
+#include <string>
 using namespace std;
+class ReadWrite {
+public:
+  int write() {
+    ifstream vault(".vault.clef");
+    if (!vault.is_open()) {
+      cerr << "File Not Found\n";
+      return 404;
+    }
+    return 0;
+  }
+  int create() {
+    ofstream vault(".vault.clef");
+    if (vault) {
+      cout << "\nNew file is created successfully";
+      return 0;
+    } else {
+      cerr << "\nFile was not created, possible problems could be lack of "
+              "permission to write";
+      exit(1);
+    }
+    return 0;
+  }
+};
 
 int main() {
-  cout << "Hello World!";
+  ReadWrite RW;
+  int filecheck = RW.write();
+  char choice = 'n';
+  if (filecheck == 404) {
+    cout << "Do you want to create a new vault? (y/n): ";
+    cin >> choice;
+  }
+  if (choice == 'y' || choice == 'Y') {
+    RW.create();
+  } else {
+    exit(1);
+  }
   return 0;
 }
